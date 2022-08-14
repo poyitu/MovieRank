097e30cb95a26a51e31f997da9a4e6e2accae049
易鹏 <108256738+ItoTakashi17@users.noreply.github.com>
2022-08-14 10:37:55 +0800

    在onCreate方法中增加了支持沉浸式状态栏代码
    要置顶图片，就没有改状态栏颜色，所以电影页面和电视剧页面有点怪，添加了图片之后就正常了，综艺页面。

--- app/src/main/java/com/qxy/movierank/MainActivity.java
+++ app/src/main/java/com/qxy/movierank/MainActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -41,6 +45,15 @@ public class MainActivity extends AppCompatActivity {
         switchFragment(mMovieFragment);
         mNavigationView.setOnItemSelectedListener(navView);
 
+
+        //实现沉浸式状态栏
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.TRANSPARENT);
+
+
     }
 
     private NavigationBarView.OnItemSelectedListener navView = new OnItemSelectedListener() {
@@ -71,5 +84,7 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container, fragment).commitNow();
     }
+
+
 
 }
