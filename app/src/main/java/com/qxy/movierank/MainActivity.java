eae77d11c7e2f46196a0ec5acae4dab6d4919314
pluto <1228782802@qq.com>
2022-08-14 10:54:59 +0800

    MainActivity，增加在onStart()中获取Client_Token

--- app/src/main/java/com/qxy/movierank/MainActivity.java
+++ app/src/main/java/com/qxy/movierank/MainActivity.java
@@ -20,9 +20,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener;
 import com.google.android.material.navigation.NavigationView;
+import com.qxy.movierank.bean.ClientTokenBean;
 import com.qxy.movierank.ui.MovieFragment;
 import com.qxy.movierank.ui.TVseriesFragment;
 import com.qxy.movierank.ui.VarietyShowFragment;
+import com.qxy.movierank.utils.RetrofitUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -85,6 +87,23 @@ public class MainActivity extends AppCompatActivity {
         transaction.replace(R.id.container, fragment).commitNow();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
 
+        RetrofitUtil.getInstance().getClientToken(new RetrofitUtil.CallBack() {
+            @Override
+            public void onSuccess(Object obj) {
+                //获取到Client_Token
+                if(((ClientTokenBean)obj).getData().getError_code() == 0){
+                    RetrofitUtil.client_access_token = ((ClientTokenBean)obj).getData().getAccess_token();
+                }
+            }
 
+            @Override
+            public void onFailed(Throwable t) {
+
+            }
+        });
+    }
 }
